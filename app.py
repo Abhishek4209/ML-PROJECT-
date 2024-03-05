88338da3af01c5cff377a960406f5fcfd6e50104
Abhishek Upadhyay <upadhyayabhishekkumar45@gmail.com>
2024-03-05 11:44:23 +0530

    data transformation

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from src.MLProject.exception import CustomException
 import sys
 from src.MLProject.components.data_ingestion import DataIngestion
 from src.MLProject.components.data_ingestion import DataIngestionConfig
+from src.MLProject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.MLProject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 
 
@@ -16,7 +18,10 @@ if __name__ == '__main__':
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
         
         
         
